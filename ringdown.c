26e3dd229a854fcedaf3b543a05686e4151da170
Chris Gerlinsky <chris@nopnop.ca>
2024-02-22 16:55:58 -0800

    adjust bot detect timeout log message

--- ringdown.c
+++ ringdown.c
@@ -325,8 +325,7 @@ void passthru_connection( int srcfd, struct sockaddr_in srcaddress, int destfd, 
             }
             else if( do_bot_detect )
             {   // bot_detect_time has expired
-                if( serve_client_args->bytes_tx < 1 )
-                    flog( LOG_DEBUG, "bot_detect_time expired with no data from source" );
+                flog( LOG_DEBUG, "bot_detect_time expired, %d bytes received from client", serve_client_args->bytes_tx );
             }
             else
                 do_bot_detect = 0;
